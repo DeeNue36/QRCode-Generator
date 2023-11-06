9728161fdac6fad2fcd28c489d2c1a8adde55cba
Dugidem Egwuatu <Dugidemeg@gmail.com>
2023-11-06 19:28:24 +0100

    full commit

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ let qrImage = document.getElementById('qr-image');
 let qrText = document.getElementById('qr-text');
 
 function generateQRCode (){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    if(qrText.value.length > 0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 
+        qrCode.classList.add('show-img');
+    }
+    else{
+        qrText.classList.add('error-msg');
+        setTimeout(() => {
+            qrText.classList.remove('error-msg')
+        }, 1000 );
+    }
+    
 }
